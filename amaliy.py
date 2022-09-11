4acdb504815d8120825721471d11508a53ad7ddf
M-Orziboyev <orziboyevmuzaffar@gmail.com>
2022-09-11 16:42:45 +0500

    Python-practice_2

---
+++ amaliy.py
@@ -0,0 +1,35 @@
+#### 2-AMALIYOT
+#SAVOL
+# "Hello world" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring!
+
+#JAVOB
+salom = "Hello world"
+
+print(salom)
+
+#SAVOL
+#xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib yana konsolga chiqaring!
+
+#JAVOB
+
+xabar = "This is paragraph"
+print(xabar)
+xabar = "this is  second paragraph"
+print(xabar)
+
+#SAVOL 
+#class deb nomlangan o'zgaruvchi yarating va unga qiymat berib konsolga chiqaring (Siz o'yalagan narsa chiqdimi?)!
+
+#JAVOB
+#class = "this word into uzbek is 'sinf'"
+#print(class) YO"Q chunki class o'zgaruvchi nomidaka kalit so'z bor
+
+
+#Quyidagi kodni matn muharririga yozing va bajaring!
+
+radius = 5
+pi = 3.14
+yuza = pi * radius**2
+print("Radiusi ", radius, "ga teng doira yuzi = ", yuza)
+#JAVOB 
+#Radiusi 5ga teng doira yuzi = 49.298
